3a0c878e541b53593affe3a87d329209775d207c
Aks9111hat <83699665+Aks9111hat@users.noreply.github.com>
2024-09-08 01:47:15 +0530

    interface test

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,11 @@ export function middleware(request: NextRequest) {
 
     const token = request.cookies.get('token')?.value || '';
 
+    const response = NextResponse.next();
+    response.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
     if (isPublicPath && token) {
         return NextResponse.redirect(new URL('/profile', request.nextUrl))
     }
